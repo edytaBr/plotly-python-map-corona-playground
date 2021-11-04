15d415cab0f16357ba862e2c9f017806acc7c8aa
edytaBr <ebredla@gmail.com>
2021-11-04 13:20:16 +0100

    add updated projedt with color map and cleaned code

--- EX7.py
+++ EX7.py
@@ -5,64 +5,12 @@ Created on Mon Nov  1 11:40:27 2021
 
 @author: edyta
 """
-from sklearn.metrics import accuracy_score
-import pandas as pd
-import numpy as np
-from sklearn.neighbors import KNeighborsClassifier
 
-ratings = pd.read_csv('ratings.csv', decimal=',')
-item = pd.read_csv('item_based.csv', decimal=',')
-coll = ratings - ratings.mean()
-coll.drop(['Unnamed: 0'], axis=1, inplace=True)
-
-corr = round(coll.corr(method = 'pearson'), 2)
-collaborative = pd.read_csv('collaborative.csv',  decimal=",")
-
-# %%
-import pandas as pd
-def openSheetsXlxs(name):
-    file = pd.ExcelFile(name)
-    listSheets = file.sheet_names
-    for elem in listSheets:
-         globals()[elem] = pd.read_excel(name, sheet_name=elem)
-         globals()[elem].drop(['Unnamed: 0'], axis=1, inplace=True)
-
-    return globals()[elem]
-
-openSheetsXlxs("traffic_rules.xlsx")
-
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-model = LinearRegression()
-
-
-#Ordinal
-train, test = train_test_split(rules_ordinal, test_size=0.2)
-X = train.drop([train.columns[-1]], axis=1)
-Y = train.drop(train.columns[0:-1], axis=1) #predict pass
-
-X_test = train.drop([test.columns[-1]], axis=1)
-Y_test = train.drop(test.columns[0:-1], axis=1) #predict pass
-model.fit(X,Y)
-prediction = model.predict(X_test)
-print("Ordinary mse " , mean_squared_error(Y_test,prediction))
-
-#Categorical
-train, test = train_test_split(rules_categorical, test_size=0.2)
-X = train.drop([train.columns[-1]], axis=1)
-Y = train.drop(train.columns[0:-1], axis=1) #predict pass
-
-X_test = train.drop([test.columns[-1]], axis=1)
-Y_test = train.drop(test.columns[0:-1], axis=1) #predict pass
-model.fit(X,Y)
-prediction = model.predict(X_test)
-print("Categorical mse " , mean_squared_error(Y_test,prediction))
-
-# %%
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
+import plotly.express as px
+import plotly
 pio.renderers.default = "browser"
 
 c = pd.read_csv('time_series_covid_19_confirmed.csv', decimal='.')
@@ -93,5 +41,6 @@ data['Deaths'] = data2['SumD'].values
 px.set_mapbox_access_token(open("a.mapbox_token").read())
 df = data
 fig = px.scatter_mapbox(df, lat="Lat", lon="Long", size="Sum",color = "Deaths",
-                  color_continuous_scale=px.colors.cyclical.IceFire, size_max=100, zoom=1.5)
+                  color_continuous_scale='RdBu_r', size_max=100, zoom=1.5)
 fig.show()
+plotly.offline.plot(fig, filename='/home/edyta/git/plotly-python-map-corona-playground/index.html')
